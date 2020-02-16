b084c87fe044316d1d434981f54a413488e5109b
bzsub <bzsub@gmail.com>
2020-02-16 16:30:59 +0100

    Updates

---
+++ precache-manifest.af93049379ee794db2a3fd188396628c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef51db220943d8849ba335ee2ec308ed",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "05de623213dda8322db1",
+    "url": "/calculator/static/css/main.0b9d57f7.chunk.css"
+  },
+  {
+    "revision": "d6a0dd44b3acd16099ef",
+    "url": "/calculator/static/js/2.6808a974.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.6808a974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05de623213dda8322db1",
+    "url": "/calculator/static/js/main.720225ff.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
